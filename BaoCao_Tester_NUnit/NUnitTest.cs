2fed65478ed795b5a3de5cbcf547382fb8feb8f1
trantrung <trung32004@gmail.com>
2025-04-03 01:34:11 +0700

    UPDATE

--- BaoCao_Tester_NUnit/NUnitTest.cs
+++ BaoCao_Tester_NUnit/NUnitTest.cs
@@ -1,20 +1,19 @@
-﻿using nu = NUnit.Framework;
-using mic = Microsoft.VisualStudio.TestTools.UnitTesting;
-using BaoCao_KTPM_GK;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using NUnit.Framework;
 using Excel = Microsoft.Office.Interop.Excel;
+using BaoCao_KTPM_GK;
 using static BaoCao_KTPM_GK.Form1;
 
 namespace BaoCao_Tester_NUnit
 {
-    [nu.TestFixture]
+    [TestFixture]
     public class UnitTest1_62_Trung
     {
-        public static IEnumerable<nu.TestCaseData> DocDuLieuTuExcel_62_Trung()
+        public static IEnumerable<TestCaseData> DocDuLieuTuExcel_62_Trung()
         {
-            string filePath = Path.Combine(nu.TestContext.CurrentContext.TestDirectory, "Data", "TestData.xlsx");
+            string filePath = Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "TestData_Excel.xlsx");
 
             var excelApp = new Excel.Application();
             var workbook = excelApp.Workbooks.Open(filePath);
@@ -35,14 +34,14 @@ namespace BaoCao_Tester_NUnit
                 double y4_62_Trung = (double)(usedRange.Cells[dong_62_Trung, 8] as Excel.Range).Value2;
                 double dienTichKyVong_62_Trung = (double)(usedRange.Cells[dong_62_Trung, 9] as Excel.Range).Value2;
 
-                yield return new nu.TestCaseData(x1_62_Trung, y1_62_Trung, x2_62_Trung, y2_62_Trung, x3_62_Trung, y3_62_Trung, x4_62_Trung, y4_62_Trung).Returns(dienTichKyVong_62_Trung);
+                yield return new TestCaseData(x1_62_Trung, y1_62_Trung, x2_62_Trung, y2_62_Trung, x3_62_Trung, y3_62_Trung, x4_62_Trung, y4_62_Trung).Returns(dienTichKyVong_62_Trung);
             }
 
             workbook.Close(false);
             excelApp.Quit();
         }
 
-        [nu.Test, nu.TestCaseSource(nameof(DocDuLieuTuExcel_62_Trung))]
+        [Test, TestCaseSource(nameof(DocDuLieuTuExcel_62_Trung))]
         public double KiemThuTinhDienTich_62_Trung(double x1_62_Trung, double y1_62_Trung, double x2_62_Trung, double y2_62_Trung,
                                                    double x3_62_Trung, double y3_62_Trung, double x4_62_Trung, double y4_62_Trung)
         {
